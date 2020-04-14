9768c6de257a43ba94c1df85a238150174810969
cirno <metroyuan@gmail.com>
2020-04-14 20:26:54 +0800

    Init for Utility

---
+++ GoPractice.Util/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace GoPractice.Util
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
