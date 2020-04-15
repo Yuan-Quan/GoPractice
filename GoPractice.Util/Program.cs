d5ee75e371801f297fe857d96135e774f5d9cbd8
cirno <metroyuan@gmail.com>
2020-04-15 18:54:05 +0800

    Start coding!!

--- GoPractice.Util/Program.cs
+++ GoPractice.Util/Program.cs
@@ -1,16 +1,44 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.IO;
+using CommandDotNet;
 using GoPractice.Util;
 
 namespace GoPractice.Util
 {
     class Program
     {
-        static void Main(string[] args)
+        static int Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
-            Console.ReadKey();
+            return new AppRunner<MainEntry>()
+            .UseDefaultMiddleware()
+            .Run(args);
         }
+    }
+
+
+
+    public class MainEntry
+    {
+        [Command(Name = "new",
+        Usage = "new [date]",
+        Description = "creat a new report",
+        ExtendedHelpText = "creat a new report,\nspecify [date] to customize the file name.\ncustom template or file path not implemented")]
+        public void CreatNewReport(
+            [Option(ShortName = "d")]string date = null
+            )
+        {
+            if (date == null)
+            {
+                Console.WriteLine("No date specifyed, using current date");
+                //maybe print the current timezone 
+                date = Util.GetDateString(DateTime.Now);
+            }
+            
+            Console.WriteLine($"new record will be named {date}.md");
+
+
+        }
+           
     }
 }
--- GoPractice.Util/Util.cs
+++ GoPractice.Util/Util.cs
@@ -5,11 +5,11 @@ using System.Text;
 
 namespace GoPractice.Util
 {
-    class Util
+    public class Util
     {
         /// <summary>
         /// Reading and Echoing the File
-        /// <br?
+        /// <br></br>
         /// Each time the calling code requests the next item from the sequence, the code reads the next line of text from the file and returns it.
         /// </summary>
         /// <param name="file">Path of the file</param>
@@ -23,5 +23,62 @@ namespace GoPractice.Util
                 yield return line;
             }
         }
+
+        public static string GetDateString(DateTime dt)
+        {
+            var month = dt.Month;
+            var day = dt.Day;
+            var year = dt.Year;
+
+
+            return ""+MonthToString(month)+day+"."+year;
+        }
+
+
+        private static string MonthToString(int month)
+        {
+            switch (month)
+            {
+                case 1:
+                    return "Jan.";
+                    break;
+                case 2:
+                    return "Feb.";
+                    break;
+                case 3:
+                    return "Mar.";
+                    break;
+                case 4:
+                    return "Apr.";
+                    break;
+                case 5:
+                    return "May";
+                    break;
+                case 6:
+                    return "June";
+                    break;
+                case 7:
+                    return "July";
+                    break;
+                case 8:
+                    return "Aug.";
+                    break;
+                case 9:
+                    return "Sept.";
+                    break;
+                case 10:
+                    return "Oct.";
+                    break;
+                case 11:
+                    return "Nov.";
+                    break;
+                case 12:
+                    return "Dec.";
+                    break;
+                default:
+                    return "err";
+                    break;
+            }
+        } 
     }
 }
