af0c642def3d1ab357577df727a917354903ee5c
cirno <metroyuan@gmail.com>
2020-04-16 19:30:57 +0800

    Finished [new] feature!!

--- GoPractice.Util/Program.cs
+++ GoPractice.Util/Program.cs
@@ -44,37 +44,119 @@ namespace GoPractice.Util
             
             Console.WriteLine($"new record will be named {date}.md");
 
-            if (File.Exists(@$"{path}/src/records/templates.md"))
+            if (File.Exists(@$"{path}/src/records/{date}.md"))
             {
-                File.Delete(@$"{path}/src/records/templates.md");
-                Console.Write($"{path}/src/records/templates.md exitsted, will ");
+                //file already exists
+
+                Console.Write($"{path}/src/records/templates.md already exitsts, ");
+
+                Console.WriteLine("\n");
+                preForegroundColor = Console.ForegroundColor;
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.Write("[1]SKIP");
+                Console.ForegroundColor = preForegroundColor;
+                Console.Write(" break the new opreation.");
+                Console.WriteLine();
+
+                preForegroundColor = Console.ForegroundColor;
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.Write("[2]DELETE:");
+                Console.ForegroundColor = preForegroundColor;
+                Console.Write(" will delete it.");
+                Console.WriteLine();
+
+                preForegroundColor = Console.ForegroundColor;
+                Console.ForegroundColor = ConsoleColor.Yellow;
+                Console.Write("[3]OVERWRITE:");
+                Console.ForegroundColor = preForegroundColor;
+                Console.Write(" will delete it, then creat a new one.");
+                Console.WriteLine();
+
+                while (true)
+                {
+
+                    Console.Write("Select a option -> ");
+
+                    if (Int32.TryParse(Console.ReadLine(), out int result))
+                    {
+                        switch (result)
+                        {
+                            case 2:
+                                DeleteExist();
+                                return;
+                            case 1:
+                                Console.WriteLine("will stop opreation 'new'.");
+                                return;
+                            case 3:
+                                DeleteExist();
+                                GenerateReport();
+                                return;
+                            default:
+                                Console.WriteLine("No this option!!");
+                                continue;
+                        }
+                    }
+                    else
+                    {
+                        Console.WriteLine("Not a valid input!!");
+                        continue;
+                    }
+
+                }
+            }
+            else
+            {
+                //no exist file
+
+                GenerateReport();
+
+                return;
+            }
+
+            //Delete duplicate file
+            void DeleteExist()
+            {
+                Console.Write($"Will ");
                 preForegroundColor = Console.ForegroundColor;
                 Console.ForegroundColor = ConsoleColor.Red;
                 Console.Write("DELETE");
                 Console.ForegroundColor = preForegroundColor;
-                Console.Write(" it.");
+                Console.Write(@$" {path}/src/records/{date}.md");
                 Console.WriteLine();
-            }
 
-            try
-            {
-                File.Copy(@$"{path}/src/templates/DailyReport.md", @$"{path}/src/records/test01.md");
-            }
-            catch (DirectoryNotFoundException)
-            {
+                File.Delete(@$"{path}/src/records/{date}.md");
+
                 preForegroundColor = Console.ForegroundColor;
-                Console.ForegroundColor = ConsoleColor.Red;
-                Console.Write("WRONG PATH!! Have you set the right path in app.config???");
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.Write("Done!");
                 Console.ForegroundColor = preForegroundColor;
                 Console.WriteLine();
-                throw;
             }
 
-            ConsoleColor preConsoleColor = Console.ForegroundColor;
-            Console.ForegroundColor = ConsoleColor.Green;
-            Console.Write($"File {date}.md generated successfully");
-            Console.ForegroundColor = preConsoleColor;
-            Console.WriteLine();
+            //New a report
+            void GenerateReport()
+            {
+                try
+                {
+                    File.Copy(@$"{path}/src/templates/DailyReport.md", @$"{path}/src/records/{date}.md");
+                }
+                catch (DirectoryNotFoundException)
+                {
+                    preForegroundColor = Console.ForegroundColor;
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.Write("WRONG PATH!! Have you set the right path in app.config???");
+                    Console.ForegroundColor = preForegroundColor;
+                    Console.WriteLine();
+                    throw;
+                }
+
+
+                ConsoleColor preConsoleColor = Console.ForegroundColor;
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.Write($"File {date}.md generated successfully");
+                Console.ForegroundColor = preConsoleColor;
+                Console.WriteLine();
+            }
         }
 
     }
