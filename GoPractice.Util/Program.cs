ff48d942d4378ea6dc5c09153cef2f037fa1dc7d
cirno <metroyuan@gmail.com>
2020-04-29 20:48:30 +0800

    Added today's report

--- GoPractice.Util/Program.cs
+++ GoPractice.Util/Program.cs
@@ -20,7 +20,7 @@ namespace GoPracticeCli
             //System.Console.WriteLine(MyUtil.GetLastFirstDate().ToShortDateString());
             //var m = new MainEntry();
             //m.EditFile();
-            System.Console.WriteLine(MyUtil.GetLineOfDate(new DateTime(2020, 4, 20)));
+            //System.Console.WriteLine(MyUtil.GetLineOfDate(new DateTime(2020, 4, 20)));
             return new AppRunner<MainEntry>()
             .UseDefaultMiddleware()
             .Run(args);
